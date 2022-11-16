f26b36660ce2a5b2a62428ca6915bb521fefb7e9
lardam <felicitasgoyaprieto@airdefelicitas.home>
2022-11-16 01:38:35 -0300

    Fix 1440p + Google Search Console - v1.2

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
         <title>ECOAL Trading</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico"/>
+        <meta name="google-site-verification" content="X4ndxqPZR4vVE6vagFmVlPI1Ln965c4QSybU56O6Ngo" />
         <meta name="description" content="ECOAL Trading, empresa dedicada a facilitar el intercambio y distribución de bienes" />
         <meta name="keywords" content="ECOAL Trading, ECOAL, Trading, comercio, intercambio, bienes, confiable" />
       </head>
